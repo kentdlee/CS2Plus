8c45049380c97d4166c5a6701de1df7b653481c9
Kent Lee <kentdlee@gmail.com>
2023-01-16 10:58:50 -0600

    added a little file handling exception code to reading file

--- source/chap2/files/PlotData.py
+++ source/chap2/files/PlotData.py
@@ -2,6 +2,7 @@ import turtle
 import tkinter
 import tkinter.colorchooser
 import tkinter.filedialog
+import tkinter.messagebox
 import xml.dom.minidom
 import math
 import sys
@@ -13,152 +14,160 @@ class PlotApplication(tkinter.Frame):
         self.pack()
         self.buildWindow()
 
- 
+
     def buildWindow(self):
-         
+
         self.master.title("Plot")
 
         bar = tkinter.Menu(self.master)
         fileMenu = tkinter.Menu(bar,tearoff=0)
-            
+
         def loadFile(filename=None):
 
             if filename == None:
                 filename = tkinter.filedialog.askopenfilename(title="Select a Plot File")
-            
-            theTurtle.clear()
-            theTurtle.penup()
-            theTurtle.goto(0,0)
-            theTurtle.pendown()  
-            screen.update()
-            theTurtle.color("black")
-            
-            xmldoc = xml.dom.minidom.parse(filename)
-                    
-            plotElement = xmldoc.getElementsByTagName("Plot")[0]
-            
-            attr = plotElement.attributes
-            self.master.title(attr["title"].value)
 
-            axesElement = plotElement.getElementsByTagName("Axes")[0]
-            
-            xAxisElement = axesElement.getElementsByTagName("XAxis")[0]
-            xAxisLabel = xAxisElement.firstChild.data.strip()
-             
-            yAxisElement = axesElement.getElementsByTagName("YAxis")[0]
-            yAxisLabel = yAxisElement.firstChild.data.strip()
-            
-            xAttr = xAxisElement.attributes
-            yAttr = yAxisElement.attributes
-            
-            minX = float(xAttr["min"].value)
-            maxX = float(xAttr["max"].value)
-            minY = float(yAttr["min"].value)
-            maxY = float(yAttr["max"].value)
-            
-            xSize = maxX - minX
-            ySize = maxY - minY
-            xCenter = xSize / 2.0 + minX
-            yCenter = ySize / 2.0 + minY
-            
-            xPlaces = max(4-round(math.log(xSize,10)),0)
-            yPlaces = max(4-round(math.log(ySize,10)),0)
-            
-            labelYVal = maxY - 0.10 * ySize
-            
-            screen.setworldcoordinates(minX-0.20 * xSize,minY - 0.20 * ySize, \
-                maxX + 0.20 * xSize,maxY + 0.20 * ySize)
+            if filename == None or filename == '':
+                return
 
-            theTurtle.ht()
-            
-            theTurtle.penup()
-            theTurtle.goto(minX,minY)
-            theTurtle.pendown()
-            theTurtle.goto(maxX,minY)
-            theTurtle.penup()
-            theTurtle.goto(minX,minY)
-            theTurtle.pendown()
-            theTurtle.goto(minX,maxY)
-            theTurtle.penup()
+            try:
 
-            theTurtle.goto(xCenter, minY - ySize * 0.10)
-            theTurtle.write(xAxisLabel,align="center",font=("Arial",14,"bold"))            
-
-            theTurtle.goto(minX, maxY + 0.05 * ySize)
-            theTurtle.write(yAxisLabel,align="center",font=("Arial",14,"bold"))
-            
-            for i in range(0,101,10):
-                x = minX + xSize * i / 100.0
-                y = minY + ySize * i / 100.0
-                
+                theTurtle.clear()
                 theTurtle.penup()
-                theTurtle.goto(x,minY+ySize * 0.025)
+                theTurtle.goto(0,0)
                 theTurtle.pendown()
-                theTurtle.goto(x,minY-ySize * 0.025)
+                screen.update()
+                theTurtle.color("black")
+
+                xmldoc = xml.dom.minidom.parse(filename)
+
+                plotElement = xmldoc.getElementsByTagName("Plot")[0]
+
+                attr = plotElement.attributes
+                self.master.title(attr["title"].value)
+
+                axesElement = plotElement.getElementsByTagName("Axes")[0]
+
+                xAxisElement = axesElement.getElementsByTagName("XAxis")[0]
+                xAxisLabel = xAxisElement.firstChild.data.strip()
+
+                yAxisElement = axesElement.getElementsByTagName("YAxis")[0]
+                yAxisLabel = yAxisElement.firstChild.data.strip()
+
+                xAttr = xAxisElement.attributes
+                yAttr = yAxisElement.attributes
+
+                minX = float(xAttr["min"].value)
+                maxX = float(xAttr["max"].value)
+                minY = float(yAttr["min"].value)
+                maxY = float(yAttr["max"].value)
+
+                xSize = maxX - minX
+                ySize = maxY - minY
+                xCenter = xSize / 2.0 + minX
+                yCenter = ySize / 2.0 + minY
+
+                xPlaces = max(4-round(math.log(xSize,10)),0)
+                yPlaces = max(4-round(math.log(ySize,10)),0)
+
+                labelYVal = maxY - 0.10 * ySize
+
+                screen.setworldcoordinates(minX-0.20 * xSize,minY - 0.20 * ySize, \
+                    maxX + 0.20 * xSize,maxY + 0.20 * ySize)
+
+                theTurtle.ht()
+
                 theTurtle.penup()
-                theTurtle.goto(x,minY-ySize * 0.05)
-                
-                theTurtle.write(("%1."+str(xPlaces)+"f")%x,align="center", \
-                    font=("Arial",12,"normal"))
-                
-                theTurtle.penup()
-                theTurtle.goto(minX+xSize * 0.025, y)
+                theTurtle.goto(minX,minY)
                 theTurtle.pendown()
-                theTurtle.goto(minX-xSize * 0.025, y)
-                theTurtle.goto(minX-xSize * 0.001, y)
-                theTurtle.write(("%1."+str(yPlaces)+"f")%y,align="right", \
-                    font=("Arial",12,"normal"))
-                 
-            
-            sequences = plotElement.getElementsByTagName("Sequence")
-            
-            for sequence in sequences:
-                attr = sequence.attributes
-                
-                label = attr["title"].value.strip()
-                color = attr["color"].value
-                theTurtle.color(color)
+                theTurtle.goto(maxX,minY)
                 theTurtle.penup()
-                theTurtle.goto(xCenter,labelYVal)
-                labelYVal = labelYVal - 0.10 * ySize
-                theTurtle.write(label,align="center",font=("Arial",14,"bold"))
-                
-                dataPoints = sequence.getElementsByTagName("DataPoint")
-                
-                first = dataPoints[0]
-                attr = first.attributes
-                x = float(attr["x"].value)
-                y = float(attr["y"].value)
-                theTurtle.goto(x,y)
-                theTurtle.dot()
+                theTurtle.goto(minX,minY)
                 theTurtle.pendown()
-                
-                for dataPoint in dataPoints:
-                    attr = dataPoint.attributes
+                theTurtle.goto(minX,maxY)
+                theTurtle.penup()
+
+                theTurtle.goto(xCenter, minY - ySize * 0.10)
+                theTurtle.write(xAxisLabel,align="center",font=("Arial",14,"bold"))
+
+                theTurtle.goto(minX, maxY + 0.05 * ySize)
+                theTurtle.write(yAxisLabel,align="center",font=("Arial",14,"bold"))
+
+                for i in range(0,101,10):
+                    x = minX + xSize * i / 100.0
+                    y = minY + ySize * i / 100.0
+
+                    theTurtle.penup()
+                    theTurtle.goto(x,minY+ySize * 0.025)
+                    theTurtle.pendown()
+                    theTurtle.goto(x,minY-ySize * 0.025)
+                    theTurtle.penup()
+                    theTurtle.goto(x,minY-ySize * 0.05)
+
+                    theTurtle.write(("%1."+str(xPlaces)+"f")%x,align="center", \
+                        font=("Arial",12,"normal"))
+
+                    theTurtle.penup()
+                    theTurtle.goto(minX+xSize * 0.025, y)
+                    theTurtle.pendown()
+                    theTurtle.goto(minX-xSize * 0.025, y)
+                    theTurtle.goto(minX-xSize * 0.001, y)
+                    theTurtle.write(("%1."+str(yPlaces)+"f")%y,align="right", \
+                        font=("Arial",12,"normal"))
+
+
+                sequences = plotElement.getElementsByTagName("Sequence")
+
+                for sequence in sequences:
+                    attr = sequence.attributes
+
+                    label = attr["title"].value.strip()
+                    color = attr["color"].value
+                    theTurtle.color(color)
+                    theTurtle.penup()
+                    theTurtle.goto(xCenter,labelYVal)
+                    labelYVal = labelYVal - 0.10 * ySize
+                    theTurtle.write(label,align="center",font=("Arial",14,"bold"))
+
+                    dataPoints = sequence.getElementsByTagName("DataPoint")
+
+                    first = dataPoints[0]
+                    attr = first.attributes
                     x = float(attr["x"].value)
                     y = float(attr["y"].value)
                     theTurtle.goto(x,y)
                     theTurtle.dot()
-                    
-                screen.update()
+                    theTurtle.pendown()
 
-            
-            
+                    for dataPoint in dataPoints:
+                        attr = dataPoint.attributes
+                        x = float(attr["x"].value)
+                        y = float(attr["y"].value)
+                        theTurtle.goto(x,y)
+                        theTurtle.dot()
+
+                    screen.update()
+            except Exception as ex:
+                tkinter.messagebox.showerror('Error Reading File', 'There was an error reading the XML plot data:\n' + str(ex))
+                print("The error from reading the plot data.")
+                print(repr(ex))
+
+
         fileMenu.add_command(label="Load Plot Data...",command=loadFile)
-        
+
         fileMenu.add_command(label="Exit",command=self.master.quit)
-        
+
         bar.add_cascade(label="File",menu=fileMenu)
-        
-        self.master.config(menu=bar)    
-        
+
+        self.master.config(menu=bar)
+
         canvas = tkinter.Canvas(self,width=1000,height=800)
         canvas.pack(side=tkinter.LEFT)
-        
+
         theTurtle = turtle.RawTurtle(canvas)
-         
+
         screen = theTurtle.getscreen()
-        
+
         screen.tracer(0)
 
         if self.datafile != None:
@@ -169,10 +178,10 @@ def main():
     datafile = None
     if len(sys.argv) > 1:
         datafile = sys.argv[1]
-    plotApp = PlotApplication(root, datafile)  
+    plotApp = PlotApplication(root, datafile)
 
     plotApp.mainloop()
     print("Program Execution Completed.")
-        
+
 if __name__ == "__main__":
     main()
